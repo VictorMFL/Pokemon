85862fa09eef63b006dd9e203a531c09a09af305
VictorMFL <vm859222@gmail.com>
2023-04-28 17:06:17 -0300

    repostando projeto

---
+++ src/Components/Carregamento/CarregamentoPoke.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './CarregamentoPoke.module.css'
+
+const CarregamentoPoke = () => {
+  return (
+    <div className={styles.padding}>
+    <div className={styles.carregamento}>
+      <span></span>
+    </div>
+    </div>
+  )
+}
+
+export default CarregamentoPoke
